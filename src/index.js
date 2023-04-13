aa88a29d143f88f3ef7419059f1e5ff8662568d6
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-04-13 17:39:55 +0300

    rendering information and add lazy scroll

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 //------ВИВОДИМО ТРЕНДОВІ ФІЛЬМИ ПРИ СТАРТІ------//
 import getTrendingFilms from './js/getTrendingFilms';
 import layOutListOfFilms from './js/layOutListOfFilms';
-
+import debounce from 'lodash.debounce';
+const DEBOUNCE_DELAY = 300;
+let page = 1;
+window.addEventListener('scroll', debounce(onScrollDocument, DEBOUNCE_DELAY));
 async function showTrendingFilms() {
-  let trendingFilms = await getTrendingFilms();
-  console.log(trendingFilms);
+  let trendingFilms = await getTrendingFilms(page);
   layOutListOfFilms(trendingFilms);
 }
 showTrendingFilms();
 
 //------ПОШУК ФІЛЬМІВ ЗА ПОШУКОВИМ ЗАПИТОМ------//
+async function onScrollDocument(){
+  const scroll = document.documentElement.getBoundingClientRect()
+  if(scroll.bottom < document.documentElement.clientHeight + 150){
+      page += 1;
+      let response = await getTrendingFilms(page);
+      layOutListOfFilms(response);
+    }
+  }

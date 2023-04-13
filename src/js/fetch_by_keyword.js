1ca02fa17115883fb94408a3aa31cde0164d1d67
Alexandr Hrynenko <layt.lat@ukr.net>
2023-04-13 13:39:59 +0300

    form submit
    form submit returns array by keyword

--- src/js/fetch_by_keyword.js
+++ src/js/fetch_by_keyword.js
@@ -2,16 +2,17 @@ import axios from 'axios';
 
 const API_KEY = 'f051ac50d3bfe0c3fd75f02c1ff7b688';
 const BASE_URL = 'https://api.themoviedb.org/';
-const searchInput = document.querySelector('.query-input');
+const searchInput = document.querySelector('.search-field-js');
 searchInput.addEventListener('submit', getDataFromAPI);
 
 export default async function getDataFromAPI(evt) {
-  const keyword = evt.target.value;
+  evt.preventDefault();
+  const keyword = evt.currentTarget.elements.query.value;
   try {
     const data = await axios
       .get(`${BASE_URL}3/search/movie?api_key=${API_KEY}&query='${keyword}'`)
       .then(response => {
-        console.log(response);
+        console.log(response.data);
         response.data;
       });
     return data;

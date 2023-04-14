7e7795a8b003e0be8276fd1bc54d6a218c206059
Alexandr Hrynenko <layt.lat@ukr.net>
2023-04-14 22:06:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 //------ВИВОДИМО ТРЕНДОВІ ФІЛЬМИ ПРИ СТАРТІ------//
 import getTrendingFilms from './js/getTrendingFilms';
 import layOutListOfFilms from './js/layOutListOfFilms';
-import { getDataFromAPI } from './js/fetch_by_keyword';
+import {
+  getDataFromAPI,
+  getMoreDataFromAPI,
+  page,
+} from './js/fetch_by_keyword';
 import { keyword } from './js/fetch_by_keyword';
-
 
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
-let page = 1;
+// let page = 1;
 let response = '';
 let tempValue = '';
-
-
 
 window.addEventListener('scroll', debounce(onScrollDocument, DEBOUNCE_DELAY));
 export async function showTrendingFilms() {
@@ -21,29 +22,22 @@ export async function showTrendingFilms() {
 }
 showTrendingFilms();
 
-
-
 //------ПОШУК ФІЛЬМІВ ЗА ПОШУКОВИМ ЗАПИТОМ------//
 async function onScrollDocument() {
   const scroll = document.documentElement.getBoundingClientRect();
   if (scroll.bottom < document.documentElement.clientHeight + 150) {
-    
-    page += 1;
-    if(keyword === ''){
+    // keyword = tempValue;
+    // page += 1;
+    if (keyword === '') {
       response = await getTrendingFilms(page);
-    
     }
-    tempValue = keyword;
-      if( keyword !== tempValue){
-        page = 1;
-      }
-      
-    response = await  getDataFromAPI(tempValue, page).then(data => data.results)
-      
-    
-    
+    // tempValue = keyword;
+    // if( keyword !== tempValue){
+    //   page = 1;
+    // }
+
+    response = await getMoreDataFromAPI().then(data => data.results);
+
     layOutListOfFilms(response);
   }
 }
-
-

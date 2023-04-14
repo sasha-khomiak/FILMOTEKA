a2be33f04b6718111ae0ce35073e35767f3b5267
Alexandr Hrynenko <layt.lat@ukr.net>
2023-04-14 14:58:45 +0300

    poster plug

--- src/js/layOutListOfFilms.js
+++ src/js/layOutListOfFilms.js
@@ -1,33 +1,37 @@
 import { genres } from './genres.js';
-
 export default async function layOutListOfFilms(arrayOfFilms) {
   // console.log('genres', genres);
-
 
   const gallery = document.querySelector('.gallery'); // галерея
 
   let markup = arrayOfFilms
     .map(item => {
-      const { poster_path, title, name, first_air_date, release_date, genre_ids } = item;
+      const {
+        poster_path,
+        title,
+        name,
+        first_air_date,
+        release_date,
+        genre_ids,
+      } = item;
 
-      // -----------------------// 
+      // -----------------------//
       // із нумерованого масиву жанрів отримуємо масив назв жанрів і формуємо стрінг
       let tempGenres = [];
-      
+
       genre_ids.forEach(genre => {
-        genres.forEach(({ id, name }) => { 
-          if(id === genre){
+        genres.forEach(({ id, name }) => {
+          if (id === genre) {
             tempGenres.push(name);
             console.log(tempGenres);
           }
-        })
-      })
+        });
+      });
       let tempGenresString = tempGenres.join(', ');
-
 
       // визначаємо назву нашого фільма і вносимо змінну
       // --------------///
-      
+
       let correctFilmName = '';
       if (title !== undefined) {
         correctFilmName = title;
@@ -35,7 +39,6 @@ export default async function layOutListOfFilms(arrayOfFilms) {
       if (name !== undefined) {
         correctFilmName = name;
       }
-
 
       // визначаємо дату виходу нашого фільма і лишаємо тільки рік
       let date = '';
@@ -49,15 +52,20 @@ export default async function layOutListOfFilms(arrayOfFilms) {
       if (release_date !== undefined) {
         date = release_date;
       }
-      
+      let image_src =
+        'https://cdn.icon-icons.com/icons2/931/PNG/512/empty_file_icon-icons.com_72420.png';
+      if (poster_path !== null) {
+        image_src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+      }
+
       date = date.substring(0, 4);
 
-      // рендеремо розмітку 
+      // рендеремо розмітку
       return `
       <div class="film-card">
       <a href="#">
         <div class="thumb">
-          <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="film1" loading="lazy" />
+          <img src="${image_src}" alt="film1" loading="lazy" />
         </div>
       </a>
       <div class="info">
@@ -68,7 +76,6 @@ export default async function layOutListOfFilms(arrayOfFilms) {
     })
     .join('');
 
-
-//  Вставляємо в головний блок верстки
+  //  Вставляємо в головний блок верстки
   gallery.insertAdjacentHTML('beforeend', markup);
 }

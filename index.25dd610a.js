37e66da976a5808ca0d330ff5b46d86453594f74
Furcreator <114292729+Furcreator@users.noreply.github.com>
2023-04-14 23:17:55 +0000

    Deploying to gh-pages from @ sasha-khomiak/FILMOTEKA@a4731ca39ad2ab7f249c23a2a196124a33ea19a3 🚀

---
+++ index.25dd610a.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".scrollToTopBtn");window.onscroll=function(){document.body.scrollTop>300||document.documentElement.scrollTop>300?o.style.display="block":o.style.display="none"},o.addEventListener("click",(function(){document.body.scrollTop=0,document.documentElement.scrollTop=0}));
+//# sourceMappingURL=index.25dd610a.js.map
---
+++ index.816257f2.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".scrollToTopBtn");window.onscroll=function(){document.body.scrollTop>300||document.documentElement.scrollTop>300?o.style.display="block":o.style.display="none"},o.addEventListener("click",(function(){document.body.scrollTop=0,document.documentElement.scrollTop=0}))}();
+//# sourceMappingURL=index.816257f2.js.map

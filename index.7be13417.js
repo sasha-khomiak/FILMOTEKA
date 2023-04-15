22162ef4a1ccc8202fb96dbb0f85350f23d0d35d
sasha-khomiak <113462446+sasha-khomiak@users.noreply.github.com>
2023-04-15 15:51:01 +0000

    Deploying to gh-pages from @ sasha-khomiak/FILMOTEKA@065368cc31bea9969551f8bd4b8fc74bc6514a76 🚀

---
+++ index.7be13417.js
@@ -0,0 +1,2 @@
+const{height:e}=document.querySelector(".footer").getBoundingClientRect();document.body.style.paddingTop=`${e}px`;
+//# sourceMappingURL=index.7be13417.js.map
---
+++ index.ecb298d6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".footer").getBoundingClientRect().height;document.body.style.paddingTop="".concat(e,"px")}();
+//# sourceMappingURL=index.ecb298d6.js.map

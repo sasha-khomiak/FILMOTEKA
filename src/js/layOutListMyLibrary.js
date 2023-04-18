41fd9839dc26111c4fa64e4e3646a04a8ce38d51
Oleksandr Khomiak <khomiak@gmail.com>
2023-04-18 08:04:51 +0300

    bug fixing show result

--- src/js/layOutListMyLibrary.js
+++ src/js/layOutListMyLibrary.js
@@ -1,6 +1,7 @@
-import { clearPage } from "./fetch_by_keyword";
-export default async function layOutListOfMyLib(arrayFilms) {
+import { clearPage } from './fetch_by_keyword';
+import posterPlug from '../images/poster-plug.jpg';
 
+export default async function layOutListOfMyLib(arrayFilms) {
   const gallery = document.querySelector('.gallery');
   arrayFilms.map(item => {
     const {
@@ -21,11 +22,19 @@ export default async function layOutListOfMyLib(arrayFilms) {
     });
     let genresString = genresArray.join(', ');
 
+    let posterLink;
+
+    if (poster_path === null) {
+      posterLink = posterPlug;
+    } else {
+      posterLink = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    }
+
     const dataCard = `
       <div class="film-card" >
       <a href="#">
         <div class="thumb">
-          <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="film1" loading="lazy" data-id="${id}" />
+          <img src="${posterLink}" alt="film1" loading="lazy" data-id="${id}" />
         </div>
       </a>
       <div class="info">
@@ -36,6 +45,5 @@ export default async function layOutListOfMyLib(arrayFilms) {
       </div>
     </div>`;
     gallery.insertAdjacentHTML('beforeend', dataCard);
-
   });
 }

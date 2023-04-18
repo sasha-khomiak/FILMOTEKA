5b5975bd10545e841a1d546cc2145eb2b1a62c16
Iryna B <imbigdash@gmail.com>
2023-04-18 21:18:28 +0100

    added animation-for-social-icons
    deleted redundant commits  from my code

--- src/js/button-up.js
+++ src/js/button-up.js
@@ -1,14 +1,10 @@
-
-// знаходження кнопки "скрол вверх"
 const topButton = document.querySelector('.scrollToTopBtn');
 window.onscroll = function () {
   scrollFunction();
 };
 
-// вішаємо слухач події на кнопку скролу вверх
 topButton.addEventListener('click', topFunction);
 
-// функція додавання скролу в точці перелому
 function scrollFunction() {
   if (
     document.body.scrollTop > 300 ||
@@ -19,7 +15,7 @@ function scrollFunction() {
     topButton.style.display = 'none';
   }
 }
-// 
+
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;

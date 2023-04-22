bea2427422d71025ed1fc3e7fea1e9b6e8514976
Oleksandr Khomiak <khomiak@gmail.com>
2023-04-22 13:04:33 +0300

    team info changes

--- src/js/renderTeamCards.js
+++ src/js/renderTeamCards.js
@@ -1,28 +1,20 @@
 import { teamInfo } from './team-info';
 
+import ldSvgIcon from '../images/icons.svg';
+import ghSvgIcon from '../images/icons.svg';
+
 const teamCardContainer = document.querySelector('.modal-team__list');
 const teamCard = createTeamCardItems(teamInfo);
 teamCardContainer.insertAdjacentHTML('beforeend', teamCard);
 
 export function createTeamCardItems(teamInfo) {
   return teamInfo
-    .map(
-      ({
-        nameDev,
-        position,
-        photo,
-        photo170x1,
-        photo170x2,
-        ldLink,
-        ghLink,
-        ldSvg,
-        ghSvg,
-      }) => {
-        return ` <li class="modal-team__item">
+    .map(({ nameDev, position, photo170x1, photo170x2, ldLink, ghLink }) => {
+      return ` <li class="modal-team__item">
             <div class="modal-team__img-container">
               <img class="modal-team__img"
                 srcset="${photo170x1} 1x, ${photo170x2} 2x"
-                src='${photo}' alt='${nameDev}' width="200" height="150" />
+                src='${photo170x1}' alt='${nameDev}' width="200" height="150" />
             </div>
             <h2 class="team__name">${nameDev}</h2>
             <p class="team__profession">${position}</p>
@@ -30,19 +22,18 @@ export function createTeamCardItems(teamInfo) {
               <li><a class="team__social" href="${ghLink}" target="_blank"
                   rel="noreferrer noopener" aria-label="GitHub">
                   <svg class="team-icon" width="30" height="30">
-                    <use href="${ghSvg}#icon-github"></use>
+                    <use href="${ghSvgIcon}#icon-github"></use>
                   </svg>
                 </a></li>
               <li><a class="team__social" href="${ldLink}"
                   target="_blank" rel="noreferrer noopener" aria-label="Linkedin">
                   <svg class="team-icon" width="30" height="30">
-                    <use href="${ldSvg}#icon-linkedin"></use>
+                    <use href="${ldSvgIcon}#icon-linkedin"></use>
                   </svg>
                 </a></li>
             </ul>
           </li>`;
-      }
-    )
+    })
     .join('');
 }
 
--- src/js/team-info.js
+++ src/js/team-info.js
@@ -1,166 +1,123 @@
-import mobileKhomiak from '../images/team/oleksandr_team1_170_170.jpg';
-import tabletKhomiak from '../images/team/oleksandr_team1_170_170.jpg';
-import desktopKhomiak from '../images/team/oleksandr_team1_170_170@2x.jpg';
+import photoKhomiak from '../images/team/oleksandr_team1_170_170.jpg';
+import photoRetinaKhomiak from '../images/team/oleksandr_team1_170_170@2x.jpg';
 
-import mobileMikhnievych from '../images/team/daniil-team2-170-170.jpg';
-import tabletMikhnievych from '../images/team/daniil-team2-170-170.jpg';
-import descktopMikhnievych from '../images/team/daniil-team2-170-170@2x.jpg';
+import photoMikhnievych from '../images/team/daniil-team2-170-170.jpg';
+import photoRetinaMikhnievych from '../images/team/daniil-team2-170-170@2x.jpg';
 
-import mobileHrynenko from '../images/team/oleksandr-h-team3-170-170.jpg';
-import tabletHrynenko from '../images/team/oleksandr-h-team3-170-170.jpg';
-import descktopHrynenko from '../images/team/oleksandr-h-team3-170-170@2x.jpg';
+import photoHrynenko from '../images/team/oleksandr-h-team3-170-170.jpg';
+import photoRetinaHrynenko from '../images/team/oleksandr-h-team3-170-170@2x.jpg';
 
-import mobileBigdash from '../images/team/iryna-team4-170-170.jpg';
-import tabletBigdash from '../images/team/iryna-team4-170-170.jpg';
-import descktopBigdash from '../images/team/iryna-team4-170-170@2x.jpg';
+import photoBigdash from '../images/team/iryna-team4-170-170.jpg';
+import photoRetinaBigdash from '../images/team/iryna-team4-170-170@2x.jpg';
 
-import mobileAbramets from '../images/team/denys-team5-170-170.jpg';
-import tabletAbramets from '../images/team/denys-team5-170-170.jpg';
-import descktopAbramets from '../images/team/denys-team5-170-170@2x.jpg';
+import photoAbramets from '../images/team/denys-team5-170-170.jpg';
+import photoRetinaAbramets from '../images/team/denys-team5-170-170@2x.jpg';
 
-import mobileTkachuk from '../images/team/roman-team6-170-170.jpg';
-import tabletTkachuk from '../images/team/roman-team6-170-170.jpg';
-import descktopTkachuk from '../images/team/roman-team6-170-170@2x.jpg';
+import photoTkachuk from '../images/team/roman-team6-170-170.jpg';
+import photoRetinaTkachuk from '../images/team/roman-team6-170-170@2x.jpg';
 
-import mobileSokotun from '../images/team/yuliia-team7-170-170.jpg';
-import tabletSokotun from '../images/team/yuliia-team7-170-170.jpg';
-import descktopSokotun from '../images/team/yuliia-team7-170-170@2x.jpg';
+import photoSokotun from '../images/team/yuliia-team7-170-170.jpg';
+import photoRetinaSokotun from '../images/team/yuliia-team7-170-170@2x.jpg';
 
-import mobileSiukov from '../images/team/oleksandr-s-team8-170-170.jpg';
-import tabletSiukov from '../images/team/oleksandr-s-team8-170-170.jpg';
-import desktopSiukov from '../images/team/oleksandr-s-team8-170-170@2x.jpg';
+import photoSiukov from '../images/team/oleksandr-s-team8-170-170.jpg';
+import photoRetinaSiukov from '../images/team/oleksandr-s-team8-170-170@2x.jpg';
 
-import mobileDyrkach from '../images/team/natalia-team9-170-170.jpg';
-import tabletDyrkach from '../images/team/natalia-team9-170-170.jpg';
-import descktopDyrkach from '../images/team/natalia-team9-170-170@2x.jpg';
+import photoDyrkach from '../images/team/natalia-team9-170-170.jpg';
+import photoRetinaDyrkach from '../images/team/natalia-team9-170-170@2x.jpg';
 
-import mobileKononenko from '../images/team/mykola-team10-170-170.jpg';
-import tabletKononenko from '../images/team/mykola-team10-170-170.jpg';
-import descktopKononenko from '../images/team/mykola-team10-170-170@2x.jpg';
-
-import ldSvgIcon from '../images/icons.svg';
-import ghSvgIcon from '../images/icons.svg';
+import photoKononenko from '../images/team/mykola-team10-170-170.jpg';
+import photoRetinaKononenko from '../images/team/mykola-team10-170-170@2x.jpg';
 
 export const teamInfo = [
   {
     nameDev: 'Oleksandr Khomiak',
     position: 'Team-lead',
-    photo: mobileKhomiak,
-    photo170x1: tabletKhomiak,
-    photo170x2: desktopKhomiak,
+    photo170x1: photoKhomiak,
+    photo170x2: photoRetinaKhomiak,
     ldLink: 'https://www.linkedin.com/in/oleksandr-khomiak-682907125/',
     ghLink: 'https://github.com/sasha-khomiak',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Daniil Mikhnievych',
     position: 'Scrum-master',
-    photo: mobileMikhnievych,
-    photo170x1: tabletMikhnievych,
-    photo170x2: descktopMikhnievych,
+    photo170x1: photoMikhnievych,
+    photo170x2: photoRetinaMikhnievych,
     ldLink: 'https://www.linkedin.com/in/daniil-mikhnievych-6448a0270/',
     ghLink: 'https://github.com/Furcreator',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Oleksandr Hrynenko',
     position: 'Developer',
-    photo: mobileHrynenko,
-    photo170x1: tabletHrynenko,
-    photo170x2: descktopHrynenko,
+    photo170x1: photoHrynenko,
+    photo170x2: photoRetinaHrynenko,
     ldLink: 'https://www.linkedin.com/in/alex-hrynenko-8b7888272/',
     ghLink: 'https://github.com/laytlat',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Iryna Bigdash',
     position: 'Developer',
-    photo: mobileBigdash,
-    photo170x1: tabletBigdash,
-    photo170x2: descktopBigdash,
+    photo170x1: photoBigdash,
+    photo170x2: photoRetinaBigdash,
     ldLink: 'https://www.linkedin.com/in/iryna-bigdash-153814252/',
     ghLink: 'https://github.com/Iryna-Bigdash',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Denys Abramets',
     position: 'Developer',
-    photo: mobileAbramets,
-    photo170x1: tabletAbramets,
-    photo170x2: descktopAbramets,
+    photo170x1: photoAbramets,
+    photo170x2: photoRetinaAbramets,
     ldLink: 'https://www.linkedin.com/in/denys-abramets-1a5346250',
     ghLink: 'https://github.com/DenysAbramets',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Roman Tkachuk',
     position: 'Developer',
-    photo: mobileTkachuk,
-    photo170x1: tabletTkachuk,
-    photo170x2: descktopTkachuk,
+    photo170x1: photoTkachuk,
+    photo170x2: photoRetinaTkachuk,
     ldLink: 'https://www.linkedin.com/in/roman-tkachuk-b339bb25a/',
     ghLink: 'https://github.com/romadoc22',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Yuliia Sokotun',
     position: 'Developer',
-    photo: mobileSokotun,
-    photo170x1: tabletSokotun,
-    photo170x2: descktopSokotun,
+    photo170x1: photoSokotun,
+    photo170x2: photoRetinaSokotun,
     ldLink: 'https://www.linkedin.com/in/yuliia-sokotun-704590272/',
     ghLink: 'https://github.com/Yuliia1710',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Oleksandr Siukov',
     position: 'Developer',
-    photo: mobileSiukov,
-    photo170x1: tabletSiukov,
-    photo170x2: desktopSiukov,
+    photo170x1: photoSiukov,
+    photo170x2: photoRetinaSiukov,
     ldLink:
       'https://www.linkedin.com/in/%D0%B0%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80-%D1%81%D1%8E%D0%BA%D0%BE%D0%B2-6670b6272/',
     ghLink: 'https://github.com/Goingforth',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Nataliia Dyrkach',
     position: 'Developer',
-    photo: mobileDyrkach,
-    photo170x1: tabletDyrkach,
-    photo170x2: descktopDyrkach,
+    photo170x1: photoDyrkach,
+    photo170x2: photoRetinaDyrkach,
     ldLink:
       'https://www.linkedin.com/in/%D0%BD%D0%B0%D1%82%D0%B0%D0%BB%D0%B8%D1%8F-%D0%B4-082a15272',
     ghLink: 'https://github.com/NataliiaDyrkach',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 
   {
     nameDev: 'Mykola Kononenko',
     position: 'Developer',
-    photo: mobileKononenko,
-    photo170x1: tabletKononenko,
-    photo170x2: descktopKononenko,
+    photo170x1: photoKononenko,
+    photo170x2: photoRetinaKononenko,
     ldLink: 'https://www.linkedin.com/in/mykola-kononenko-16518a8b/',
     ghLink: 'https://github.com/KononenkoMykola',
-    ldSvg: ldSvgIcon,
-    ghSvg: ghSvgIcon,
   },
 ];
